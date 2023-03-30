4d49dace64bf2e88f79b5bb313892911f2530a0a
Kayengxiong TONGSUATONG <104555848+kayengxiongTST@users.noreply.github.com>
2023-03-30 13:59:25 +0700

    Get started

--- app.js
+++ app.js
@@ -1,43 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const AWS = require('aws-sdk');
-const multer = require('multer');
-const multerS3 = require('multer-s3');
 
 const User = require('./models/user');
 require('./connections/db.js')
-
-const s3 = new AWS.S3({
-	accessKeyId: '',
-	secretAccessKey: '',
-	Bucket: ''
-});
-const fileFilter = (req, file, cb) => {
-	if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-		cb(null, true)
-	} else {
-		cb(null, false)
-	}
-}
-
-const multerS3Config = multerS3({
-	s3: s3,
-	bucket: 'learn-s3-mongo',
-	metadata: function (req, file, cb) {
-		cb(null, { fieldName: file.fieldname });
-	},
-	key: function (req, file, cb) {
-		console.log(file)
-		cb(null, new Date().toISOString() + '-' + file.originalname)
-	}
-});
-const upload = multer({
-	storage: multerS3Config,
-	fileFilter: fileFilter,
-	limits: {
-		fileSize: 1024 * 1024 * 5 // we are allowing only 5 MB files
-	}
-})
 
 const app = express();
 app.use(cors())
@@ -48,12 +13,11 @@ app.get('/users', async (req, res) => {
 	res.send(user);
 });
 
-app.post('/users', upload.single('img'), async (req, res) => {
-	// const { name, username, password, phone } = req.body;
-	// const user = new User({ name, username, password, phone });
-	// const savedUser = await user.save();
-	// res.send(savedUser);
-	res.send("Done")
+app.post('/users', async (req, res) => {
+	const { name, username, password, phone } = req.body;
+	const user = new User({ name, username, password, phone });
+	const savedUser = await user.save();
+	res.send(savedUser);
 });
 
 app.use((req, res) => {
